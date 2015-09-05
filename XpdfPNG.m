e8da09acafe0c1a719ba43f28295c8720445c491
Mark Geurts <mark.w.geurts@gmail.com>
2015-09-04 21:46:47 -0500

    Update XpdfPNG.m

--- XpdfPNG.m
+++ XpdfPNG.m
@@ -115,7 +115,7 @@ end
 % Clear temporary variables
 clear status tmpName i;
 
-% Log start
+% Log finish
 if exist('Event', 'file') == 2
-    Event(sprintf('Extraction completed successfully in %0.3f seconds', toc));
+    Event(sprintf('Image extraction completed successfully in %0.3f seconds', toc));
 end
